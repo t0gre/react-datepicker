1aaf1111e2ab03d07df4deb80c75fc70d3d79145
Miha Sedej <miha.sedej@lifeonscreen.com>
2019-03-26 09:30:40 +0100

    chore(project): add storybook
    affects: @react-datepicker/hooks

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = ({config}) => {
+  config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    loader: require.resolve('awesome-typescript-loader'),
+  })
+  config.resolve.extensions.push('.ts', '.tsx')
+  return config
+}
---
+++ packages/react-datepicker-hooks/src/index.stories.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react'
+import {storiesOf} from '@storybook/react'
+import Demo from '.'
+
+storiesOf('Demo', module).add('basic', () => <Demo />)
