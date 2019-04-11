be13376b705ba38fdae741364f782a515355786f
Miha Sedej <sedej.miha@gmail.com>
2019-04-11 19:28:38 +0200

    test(styled): 100% code coverage - Flex component
    affects: @datepicker-react/styled

---
+++ packages/styled/src/components/Flex/Flex.test.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {render} from 'react-testing-library'
+import Flex from '.'
+
+test('should match snapshot', () => {
+  const {container} = render(
+    <Flex
+      height="50px"
+      width="50px"
+      gridArea="test"
+      justifyContent="center"
+      alignItems="center"
+      flexDirection="column"
+      flexWrap="wrap"
+      m="30px"
+    >
+      <div>test1</div>
+      <div>test2</div>
+      <div>test3</div>
+      <div>test4</div>
+      <div>test5</div>
+    </Flex>,
+  )
+  expect(container).toMatchSnapshot()
+})
